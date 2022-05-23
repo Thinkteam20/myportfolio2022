e98993b16cbd7eb20b6a5c41478806b0b90b5ba2
Sangpil <tankteam20@gmail.com>
2022-05-23 23:33:42 +1000

    added span

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,37 +3,50 @@ import {
     MainContainer,
     ParticleContainer,
     MainPageContents,
+    TypeCover,
+    GoNext,
 } from "./Home.styles.js";
 // import { Skill, Timeline, Footer, Projects } from "../index.js";
 import Typewriter from "typewriter-effect";
 import Background from "../../components/Background.js";
 import Navbar from "../../Navbar.js";
+import { IoMdArrowRoundDown } from "react-icons/io";
 
 function Home() {
     return (
         <MainContainer>
             <MainPageContents>
                 <Background />
-                <h1>
-                    <Typewriter
-                        onInit={(typewriter) => {
-                            typewriter
-                                .pauseFor(100)
-                                .typeString("Hi,")
-                                .pauseFor(300)
-                                .deleteChars(10)
-                                .typeString(
-                                    "I am<strong> Sangpil Jung</strong> "
-                                )
-                                .deleteChars(13)
-                                .typeString(
-                                    '<strong><span style="color: #27ae60;">Front-end Developer</span> in Australia.</strong>'
-                                )
-                                .pauseFor(100)
-                                .start();
-                        }}
-                    />
-                </h1>
+                <TypeCover>
+                    <h1>
+                        <Typewriter
+                            onInit={(typewriter) => {
+                                typewriter
+                                    .pauseFor(100)
+                                    .typeString("Hi,")
+                                    .pauseFor(300)
+                                    .deleteChars(10)
+                                    .typeString(
+                                        "I am<strong> Sangpil Jung</strong> "
+                                    )
+                                    .deleteChars(13)
+                                    .typeString(
+                                        '<strong><span style="color: #27ae60;">Front-end Developer</span> in Australia.</strong>'
+                                    )
+                                    .pauseFor(100)
+                                    .start();
+                            }}
+                        />
+                    </h1>
+                </TypeCover>
+                <GoNext>
+                    <span>Go to Next</span>
+                    <div>
+                        <span>
+                            <IoMdArrowRoundDown />
+                        </span>
+                    </div>
+                </GoNext>
             </MainPageContents>
         </MainContainer>
     );
--- src/pages/Home/Home.styles.js
+++ src/pages/Home/Home.styles.js
@@ -9,7 +9,21 @@ export const MainContainer = styled.div`
     justify-content: center;
 `;
 
-export const MainPageContents = styled.div`
-    text-alignment: center;
-    align-items: center;
+export const MainPageContents = styled.div``;
+
+export const TypeCover = styled.div`
+    position: absolute;
+    margin-left: auto;
+    margin-right: auto;
+    left: 0;
+    right: 0;
+    text-align: center;
+`;
+
+export const GoNext = styled.div`
+    position: absolute;
+    display: flex;
+    margin-left: auto;
+    margin-right: auto;
+    bottom: 100px;
 `;
